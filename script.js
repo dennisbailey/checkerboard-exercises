8aa43a4d1ca392318301ca4a1ab2e0b911edaf19
Dennis Bailey <bailey.dennis@gmail.com>
2016-01-14 15:27:38 -0700

    Got first two pieces of the checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Your JS goes here
+var body = document.getElementsByTagName('body')[0];
+body.style.margin = "0";
+body.style.padding = "0";
+
+
+var redSquare = document.createElement('div');
+
+redSquare.style.margin = "0";
+redSquare.style.padding = "0";
+redSquare.style.paddingBottom = "11.1%";
+redSquare.style.width = "11.1%"
+redSquare.style.float = "left";
+redSquare.style.backgroundColor = "red";
+
+
+var blackSquare = document.createElement('div');
+
+blackSquare.style.margin = "0";
+blackSquare.style.padding = "0";
+blackSquare.style.paddingBottom = "11.1%";
+blackSquare.style.width = "11.1%"
+blackSquare.style.float = "left";
+blackSquare.style.backgroundColor = "black";
+
+body.appendChild(redSquare);
+body.appendChild(blackSquare);
+
+var red = body.appendChild(redSquare);
+var black = body.appendChild(blackSquare);
